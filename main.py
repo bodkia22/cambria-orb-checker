20a36cee84dacea9f7373d72bb81b92e700e4dfd
bodkia <1vbodeth@gmail.com>
2025-12-17 03:55:50 +0200

    Format changes

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from datetime import datetime
 import sqlite3
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 DB_PATH = "txs_fast.db"
 orb_price = 0.0012
+
+def datetimeformat(value):
+    return datetime.fromtimestamp(value).strftime("%Y-%m-%d %H:%M:%S")
+
+templates.env.filters['datetimeformat'] = datetimeformat
+
 
 def get_wallet_data(address):
     conn = sqlite3.connect(DB_PATH)
